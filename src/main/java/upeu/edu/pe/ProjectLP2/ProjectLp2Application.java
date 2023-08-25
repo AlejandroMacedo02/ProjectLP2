06733963fb504147f79454f344fec7c76d97d414
AlejandroMacedo02 <106896409+AlejandroMacedo02@users.noreply.github.com>
2023-08-25 14:55:30 -0500

    first commit

---
+++ src/main/java/upeu/edu/pe/ProjectLP2/ProjectLp2Application.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.ProjectLP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/upeu/edu/pe/ProjectLP2/ProjectLp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.ProjectLP2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
