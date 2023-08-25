b55e269ff4f3c7dca1357b1917ddc58ff4bb232e
AlejandroMacedo02 <106896409+AlejandroMacedo02@users.noreply.github.com>
2023-08-25 15:16:30 -0500

    crud and controller complete (products)

---
+++ src/main/java/upeu/edu/pe/ProjectLP2/infrastructure/configuration/BeanConfiguration.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upeu.edu.pe.ProjectLP2.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import upeu.edu.pe.ProjectLP2.app.repository.ProductRepository;
+import upeu.edu.pe.ProjectLP2.app.service.ProductService;
+
+/**
+ *
+ * @author alejandromacedop
+ */
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public ProductService productService(ProductRepository productRepository) {
+        return new ProductService(productRepository);
+    }
+
+}
